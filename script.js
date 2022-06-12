243d1a1716349eddde0c914baffd408c85e9020d
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2022-06-12 11:08:47 -0300

    Atividade - 1 de Lógica VnW

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Marcos";
+let numero = 17;
+let ano = 2022;
+let quartaFeira = false;
+
+console.log("A subtração do ano pelo número é: ", ano - numero);
+
+console.log("A soma é: ", ano + numero);
+
+console.log("Estou mexendo no console.log");
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
